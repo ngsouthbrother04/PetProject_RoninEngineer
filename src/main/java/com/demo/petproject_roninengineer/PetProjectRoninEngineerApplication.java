25d4deaf97ae77cb163e47bdc0744bb25ffe7c2d
Nguyen Anh <nguyennam4c7@gmail.com>
2024-09-22 15:40:53 +0700

    Create PetProjectRoninEngineerApplication.java

---
+++ src/main/java/com/demo/petproject_roninengineer/PetProjectRoninEngineerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.petproject_roninengineer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProjectRoninEngineerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetProjectRoninEngineerApplication.class, args);
+    }
+
+}
