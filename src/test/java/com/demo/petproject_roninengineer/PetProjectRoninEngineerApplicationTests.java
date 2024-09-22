936e61befbdcf6f84564a886bf440301c6b865a5
Nguyen Anh <nguyennam4c7@gmail.com>
2024-09-22 15:48:30 +0700

    Create PetProjectRoninEngineerApplicationTests.java

---
+++ src/test/java/com/demo/petproject_roninengineer/PetProjectRoninEngineerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.petproject_roninengineer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectRoninEngineerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
